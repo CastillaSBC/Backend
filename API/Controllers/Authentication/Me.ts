2ad5645336be97f3dce5d252b21095432a88c0e0
Shiggy <ilarumbgim1@educacion.navarra.es>
2022-03-02 17:48:58 +0100

    perf(me): remove unnecesary database queries.

--- API/Controllers/Authentication/Me.ts
+++ API/Controllers/Authentication/Me.ts
@@ -1,15 +1,6 @@
 import { Request, Response } from "express"
-import { decode } from "jwt-simple"
-import prisma from "../../Prisma/prismaFile";
 
 export async function me(req: Request, res: Response) {
-    const token = req.headers.authorization;
-
-    const bearer = token!.split(" ")[1]
-
-    const { sub } = decode(bearer, process.env.JWT_SECRET!)
-
-    const user = await prisma.user.findUnique({ where: { id: sub } })
-
-    return res.status(200).json({ success: true, user })
+    //@ts-ignore
+    return res.status(200).json({ success: true, user: req.user})
 }
